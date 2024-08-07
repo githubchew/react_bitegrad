f70a3059fc3616aa52d285671f3d030a50642f50
lmde-git-hoychew <iammusiccchu@gmail.com>
2024-08-07 23:53:01 +0800

    69

---
+++ src/components/AddItemForm.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "../Button";
+
+export default function AddItemForm() {
+  return (
+    <form>
+      <h2>add an item</h2>
+      <input />
+      <Button>Add Item</Button>
+    </form>
+  );
+}
---
+++ src/components/Counter.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Counter() {
+  return <p> 1 /3 items packed</p>;
+}
