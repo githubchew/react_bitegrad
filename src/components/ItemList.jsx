ede97b9f535a7bec1877322df254df8343fd5730
Hubchew <iammusicchu@gmail.com>
2024-08-14 23:32:46 +0800

    72

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,9 +1,21 @@
 export default function ItemList() {
   return (
     <ul>
-      <li>good mood</li>
+      <Item />
+      <Item />
+      <Item />
       <li>passport</li>
       <li>phone charger</li>
     </ul>
+  );
+}
+
+function Item() {
+  return (
+    <li className="item">
+      <label>
+        <input type="checkbox" name="" id="" />god
+      </label>
+    </li>
   );
 }
