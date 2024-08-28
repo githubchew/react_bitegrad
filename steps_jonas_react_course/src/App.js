75da3a099b4adf52f8191382cb4733e357afd255
Hubchew <iammusicchu@gmail.com>
2024-08-28 20:59:52 +0800

    07-006

--- steps_jonas_react_course/src/App.js
+++ steps_jonas_react_course/src/App.js
@@ -7,9 +7,28 @@ const messages = [
   "Invest your new income 🤑",
 ];
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+
+  { id: 12, description: "Socks", quantity: 12, packed: true },
+];
+
 export default function App() {
+  const [items, setItems] = useState([]);
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
   return (
     <div>
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} />
+      <Stats />
       <Counter />
       <FlashCards />
       <Counter />
@@ -17,6 +36,86 @@ export default function App() {
     </div>
   );
 }
+
+// travel list app only below  --------------------------------------
+//------------------------------------------------------------
+
+function Logo() {
+  return <h1>🌴🌴 Far away 💼</h1>;
+}
+
+function Form({ onAddItems }) {
+  const [description, setDescription] = useState("charger");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    onAddItems(newItem);
+    setDescription("");
+    setQuantity(1);
+  }
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3> what you need for your trip? 😊🏄‍♂️</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList({ items, onDeleteItem }) {
+  return (
+    <div className="list">
+      <ul className="">
+        {items.map((item) => (
+          <Item item={item} onDeleteItem={onDeleteItem} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item, onDeleteItem }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity}
+        {item.description}
+      </span>
+      <button onClick={() => onDeleteItem(item.id)}> ❌ </button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      you have x items on list, you already packed X (X%)
+    </footer>
+  );
+}
+
+// Travel List app only above  ----------------------------
+
 function Steps() {
   const [step, setStep] = useState(3);
   const [isOpen, setIsOpen] = useState(true);
